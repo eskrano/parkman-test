802f8d226c404a9cc27475b6d279f394a1fd8b32
Eskrano <pommmper@gmail.com>
2023-04-11 02:49:25 +0300

    Added task details, readme, cors

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# PHP assignment 
+
+All details described in task.pdf.
+
+## Points to improve code
+
+- add eloquent model for garages in case if we need to create 
+- register normal container interface for db driver instead of string 'db'
+- add migrations for php? 
+- add interface for container proxy service
+- add flexibility for filters e.g more params / validations
+- pagination ?
+- wrap up web/index.php into some static service & routes (make code looks more clear)
+- add good CORS (now just copy-pasted from docs , sorry no time )
+
+Proposals??
--- src/web/index.php
+++ src/web/index.php
@@ -58,5 +58,18 @@ $app->get('/garages[/{criteria:owner|country|location}[/{criteriaValue}]]', func
 });
 
 
+// cors from docs
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 
 $app->run();
